ddd23eb59a0bfc6577dfcc27d7f94f0828c0dab8
Anuyash Gupta <anuyashgupta9026@gmail.com>
2025-07-17 02:53:14 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -41,26 +41,26 @@ def perform_division():
         messagebox.showerror("Input Error", "Please enter valid numerical values.")
 
 
-window = tk.Tk()
-window.title("Anuyash Calculator")
+harsh = tk.Tk()
+harsh.title("Anuyash Calculator")
 
 
-tk.Label(window, text="Enter First Number:").grid(row=0, column=0, padx=6, pady=6)
-entry_anuyash1 = tk.Entry(window)
+tk.Label(harsh, text="Enter First Number:").grid(row=0, column=0, padx=6, pady=6)
+entry_anuyash1 = tk.Entry(harsh)
 entry_anuyash1.grid(row=0, column=1, padx=6, pady=6)
 
-tk.Label(window, text="Enter Second Number:").grid(row=1, column=0, padx=6, pady=6)
-entry_anuyash2 = tk.Entry(window)
+tk.Label(harsh, text="Enter Second Number:").grid(row=1, column=0, padx=6, pady=6)
+entry_anuyash2 = tk.Entry(harsh)
 entry_anuyash2.grid(row=1, column=1, padx=6, pady=6)
 
-tk.Label(window, text="Result:").grid(row=2, column=0, padx=6, pady=6)
-result_box = tk.Entry(window)
+tk.Label(harsh, text="Result:").grid(row=2, column=0, padx=6, pady=6)
+result_box = tk.Entry(harsh)
 result_box.grid(row=2, column=1, padx=6, pady=6)
 
 
-tk.Button(window, text="+", width=6, command=perform_addition).grid(row=3, column=0, padx=5, pady=5)
-tk.Button(window, text="-", width=6, command=perform_subtraction).grid(row=3, column=1, padx=5, pady=5)
-tk.Button(window, text="×", width=6, command=perform_multiplication).grid(row=4, column=0, padx=5, pady=5)
-tk.Button(window, text="÷", width=6, command=perform_division).grid(row=4, column=1, padx=5, pady=5)
+tk.Button(harsh, text="+", width=6, command=perform_addition).grid(row=3, column=0, padx=5, pady=5)
+tk.Button(harsh, text="-", width=6, command=perform_subtraction).grid(row=3, column=1, padx=5, pady=5)
+tk.Button(harsh, text="×", width=6, command=perform_multiplication).grid(row=4, column=0, padx=5, pady=5)
+tk.Button(harsh, text="÷", width=6, command=perform_division).grid(row=4, column=1, padx=5, pady=5)
 
-window.mainloop()
+harsh.mainloop()
